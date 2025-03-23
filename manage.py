903fad606515a6d6a5195543da74b1d30f6fe1cd
mikerr1 <michaelraymond2@gmail.com>
2025-03-23 14:56:25 +0700

    Update settings module, .gitignore, and add env example
    Corrected DJANGO_SETTINGS_MODULE path in manage.py. Modified .gitignore to clean up and better handle environment-related files. Added a new config/env/.env.example file for better environment configuration sharing.

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings.main')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.main')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
